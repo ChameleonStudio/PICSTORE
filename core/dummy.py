2edf17e16f33de9c18482a390df4be7bbba0153b
Daniel Omelchenko <dpixelstudio@gmail.com>
2015-12-17 12:32:31 +0200

    Demo 3.

---
+++ core/dummy.py
@@ -0,0 +1,89 @@
+from django.utils.translation import ugettext_lazy as _
+
+
+def dummy():
+
+
+    # Class: AbstractUser
+    _('user')
+    _('users')
+    _('password')  # password
+    _('last login')  # last_login
+    _('superuser status')  # is_superuser
+    _('username')  # username
+    _('first name')  # first_name
+    _('last name')  # last_name
+    _('email address')  # email
+    _('staff status')  # is_staff
+    _('active')  # is_active
+    _('date joined')  # date_joined
+    _('groups')  # groups
+    _('user permissions')  # user_permissions
+
+    # Class: Cart
+    _('cart')
+    _('carts')
+    _('ID')  # id
+    _('user')  # user
+    _('picture')  # picture
+    _('payed')  # payed
+
+    # Class: Category
+    _('Category')
+    _('Categories')
+    _('ID')  # id
+    _('Name')  # name
+    _('Description')  # description
+    _('Icon class')  # icon_class
+    _('Super category')  # super_category
+
+    # Class: Picture
+    _('Picture')
+    _('Pictures')
+    _('ID')  # id
+    _('Created')  # created
+    _('Category')  # category
+    _('Name')  # name
+    _('Short description')  # short_description
+    _('Price')  # price
+    _('Picture')  # picture
+    _('Thumbnail')  # thumbnail
+    _('Sale')  # sale
+    _('Likes')  # likes
+    _('tags')  # tags
+
+    # Class: Sale
+    _('Sale')
+    _('Sales')
+    _('ID')  # id
+    _('Value')  # value
+    _('Description')  # description
+
+    # Class: SuperCategory
+    _('Super category')
+    _('Super categories')
+    _('ID')  # id
+    _('Name')  # name
+    _('Description')  # description
+
+    # Class: Tag
+    _('Tag')
+    _('Tags')
+    _('Name')  # name
+
+    # Class: User
+    _('user')
+    _('users')
+    _('ID')  # id
+    _('password')  # password
+    _('last login')  # last_login
+    _('superuser status')  # is_superuser
+    _('username')  # username
+    _('first name')  # first_name
+    _('last name')  # last_name
+    _('email address')  # email
+    _('staff status')  # is_staff
+    _('active')  # is_active
+    _('date joined')  # date_joined
+    _('groups')  # groups
+    _('user permissions')  # user_permissions
