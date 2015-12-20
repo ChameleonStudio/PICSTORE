613f805546d03175d5a2f60e7983fa12f0225d62
Daniel Omelchenko <dpixelstudio@gmail.com>
2015-12-21 00:44:58 +0200

    Form validation.

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext as _
 
 
 class RegistrationForm(forms.Form):
@@ -6,6 +7,15 @@ class RegistrationForm(forms.Form):
     password = forms.CharField(max_length=255)
     password2 = forms.CharField(max_length=255)
     email = forms.EmailField(max_length=255)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password")
+        if password != password2:
+            self.add_error('password2', _("Passwords are different!"))
+            self.add_error('password', _("Passwords are different!"))
+        return cleaned_data
 
 
 class LoginForm(forms.Form):
